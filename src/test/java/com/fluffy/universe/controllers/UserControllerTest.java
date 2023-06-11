fcc9c2b15e54f57cc442bf9fa05a83d8d41ade64
Maxim Rudenko <maxim.s.rudenko@gmail.com>
2023-06-11 14:53:44 +0300

    update UserController test

--- src/test/java/com/fluffy/universe/controllers/UserControllerTest.java
+++ src/test/java/com/fluffy/universe/controllers/UserControllerTest.java
@@ -2,37 +2,48 @@ package com.fluffy.universe.controllers;
 
 import com.fluffy.universe.models.User;
 import com.fluffy.universe.services.UserService;
+import com.fluffy.universe.utils.Configuration;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import java.io.File;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
+@DisplayName("Test UserController")
 public class UserControllerTest {
-
     private final Context ctx = mock(Context.class);
-
     private final Javalin app = mock(Javalin.class);
 
     @Test
+    @DisplayName("Testing signIn action")
     public void signInTest() {
+        Configuration.load(new File("application.properties"));
+        UserController userController = new UserController(this.app);
+
         User user = new User();
-        user.setPassword("fs$fFl24");
-        user.setEmail("test@email.com");
-        user.setFirstName("Max");
+        final String password = "fs$fFl24";
+        user.setPassword(UserService.encodePassword(password));
+        user.setEmail("test@mail.com");
+        user.setFirstName("Maxim");
         user.setLastName("Rudenko");
 
-        when(ctx.formParam("email")).thenReturn("Max");
-        when(ctx.formParam("email")).thenReturn("Max");
+        when(ctx.formParam("email")).thenReturn(user.getEmail());
+        when(ctx.formParam("password")).thenReturn(password);
 
         try (MockedStatic<UserService> userService = Mockito.mockStatic(UserService.class)) {
-            userService.when(() -> UserService.getUserByEmail(user.getEmail())).thenReturn(user);
+            userService.when(() -> UserService.getUserByEmail(anyString())).thenReturn(user);
+            userService.when(() -> UserService.isCorrectPassword(anyString(), anyString())).thenReturn(true);
+            userController.signIn(ctx);
 
-            (new UserController(app)).signIn(ctx);
+            Mockito.inOrder(ctx).verify(ctx, calls(1)).sessionAttribute(Mockito.anyString());
+            Mockito.inOrder(ctx).verify(ctx, calls(2)).formParam(Mockito.anyString());
+            Mockito.inOrder(ctx).verify(ctx, calls(1)).sessionAttribute(Mockito.anyString(), any(User.class));
             verify(ctx).redirect("/");
         }
     }
